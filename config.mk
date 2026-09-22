304c19748852d54d1808172c4d8b02a649f16974
Eduardo N. S. R. <vndmtrx@duck.com>
2026-09-22 16:48:06 -0300

    Atualização das recomendações de memória no config.mk

--- config.mk
+++ config.mk
@@ -10,13 +10,13 @@
 # ------------------------------------------------------------------------------
 # Opções disponíveis:
 #   - nano     : 1 Control Plane, 1 Worker, 1 NFS, 1 Load Balancer, 1 Bastion
-#                (Ideal para testes rápidos. Mínimo de ~6GB RAM / 6 vCPUs)
+#                (Ideal para testes rápidos. Mínimo de ~6.5GB RAM / 6 vCPUs)
 #
 #   - mini     : 1 Control Plane, 2 Workers, 1 NFS, 1 Load Balancer, 1 Bastion
-#                (Configuração padrão do projeto. Recomendado ~10GB RAM / 9 vCPUs)
+#                (Configuração padrão do projeto. Recomendado ~11GB RAM / 9 vCPUs)
 #
 #   - completo : 3 Control Planes (HA quorum), 2 Workers, 1 NFS, 2 Load Balancers
 #                (HA com Keepalived), 1 Bastion. (Ideal para simular produção/HA.
-#                Recomendado ~19GB RAM / 18 vCPUs)
+#                Recomendado ~19.5GB RAM / 18 vCPUs)
 #
 CLUSTER = mini
